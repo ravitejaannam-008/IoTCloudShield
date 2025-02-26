1a85b41ff838103febef36712feaedc2893ba52b
RAVITEJA ANNAM <150829995+ravitejaannam-008@users.noreply.github.com>
2025-02-26 06:39:01 -0600

    Delete SQL directory

--- SQL/CreateDB.sql
+++
@@ -1,8 +0,0 @@
-## Create the Database
-CREATE SCHEMA Store;
-
-## Create a user
-CREATE USER 'store'@'localhost' IDENTIFIED BY 'password';
-
-## Grant rights to the user to access the database
-GRANT ALL PRIVILEGES ON *.* TO 'store'@'localhost' WITH GRANT OPTION;
--- SQL/CreateUserTable.sql
+++
@@ -1,9 +0,0 @@
-create table User
-(
-    id       int auto_increment
-        primary key,
-    username varchar(100) null,
-    name     varchar(100) null,
-    email    varchar(100) null,
-    password varchar(100) null
-);
